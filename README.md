783ef22ea9637146ea130b9041c4ae8f4cc15b35
Antonio Vanegas <hpsaturn@gmail.com>
2020-05-08 00:27:42 +0200

    added sample for proccesing Vision Result and doc of it

--- README.md
+++ README.md
@@ -80,13 +80,56 @@ git submodule add https://github.com/matrix-io/protocol-buffers.git app/src/main
 
 ### Malos Driver
 
+#### Connection to MALOS VISION device
+
+If you have a malos vision device provisioned, only put the IP for have a connection and subscribe to Vision events:
+
 ```java
-    MalosDrive malosVision = new MalosDrive(MalosTarget.VISION, "192.168.178.53");
-        malosVision.subscribe(new MalosDrive.OnSubscriptionCallBack() {
-            @Override
-            public void onReceiveData(String host, byte[] data) {
-                Log.d(TAG,"onReceiveData on "+host+":");
-                Log.d(TAG,"data size:"+data.length);
-            }
-        });
+MalosDrive malosVision = new MalosDrive(MalosTarget.VISION, "192.168.178.53");
+malosVision.subscribe(new MalosDrive.OnSubscriptionCallBack() {
+    @Override
+    public void onReceiveData(String host, byte[] data) {
+
+    }
+});
 ```
+
+
+#### Vision Result proto
+
+Processing the proto Vision Events:
+
+```java
+VisionResult visionResult = VisionResult.parseFrom(data);
+Iterator<VisionEvent> it = visionResult.getVisionEventList().iterator();
+while (it.hasNext()) {
+    Log.d(TAG, "=== V I S I O N  E V E N T ===");
+    VisionEvent event = it.next();
+    Log.d(TAG, "Dwelltime:"   + event.getDwellTime());
+    Log.d(TAG, "= " + event.getTag().name() + " =");
+    printFaceRecognitionData(visionResult);
+}
+```
+
+#### Vision Data
+
+From `VisionResult` proto, you can many information, for example this:
+
+```java
+void printFaceRecognitionData(VisionResult visionResult) {
+    Iterator<RectangularDetection> rd_it = visionResult.getRectDetectionList().iterator();
+    while (rd_it.hasNext()) {
+        RectangularDetection rd = rd_it.next();
+        Iterator<FacialRecognition> fr_it = rd.getFacialRecognitionList().iterator();
+        while (fr_it.hasNext()) {
+            FacialRecognition fr = fr_it.next();
+            Log.d(TAG, "age:" + fr.getAge());
+            Log.d(TAG, "gender:" + fr.getGender().name());
+            Log.d(TAG, "emotion:" + fr.getEmotion().name());
+            Log.d(TAG, "pose pitch:" + fr.getPosePitch());
+            Log.d(TAG, "beard:" + fr.getBeard());
+        }
+    }
+}
+```
+
--- app/src/main/java/admobilize/hpsaturn/malosvision/MainActivity.java
+++ app/src/main/java/admobilize/hpsaturn/malosvision/MainActivity.java
@@ -1,12 +1,20 @@
 package admobilize.hpsaturn.malosvision;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.Iterator;
+
 import admobilize.hpsaturn.malosvision.malos.MalosDrive;
 import admobilize.hpsaturn.malosvision.malos.MalosTarget;
+import one.matrixio.proto.vision.v1.FacialRecognition;
+import one.matrixio.proto.vision.v1.RectangularDetection;
+import one.matrixio.proto.vision.v1.VisionEvent;
+import one.matrixio.proto.vision.v1.VisionResult;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,15 +30,37 @@ public class MainActivity extends AppCompatActivity {
         malosVision.subscribe(new MalosDrive.OnSubscriptionCallBack() {
             @Override
             public void onReceiveData(String host, byte[] data) {
-
-                Log.d(TAG,"onReceiveData on "+host+":");
-                Log.d(TAG,"data size:"+data.length);
-
+                try {
+                    VisionResult visionResult = VisionResult.parseFrom(data);
+                    Iterator<VisionEvent> it = visionResult.getVisionEventList().iterator();
+                    while (it.hasNext()) {
+                        Log.d(TAG, "=== V I S I O N  E V E N T ===");
+                        VisionEvent event = it.next();
+                        Log.d(TAG, "Dwelltime:"   + event.getDwellTime());
+                        Log.d(TAG, "= " + event.getTag().name() + " =");
+                        printFaceRecognitionData(visionResult);
+                    }
+                } catch (InvalidProtocolBufferException e) {
+                    e.printStackTrace();
+                }
             }
         });
-
     }
 
-
+    void printFaceRecognitionData(VisionResult visionResult) {
+        Iterator<RectangularDetection> rd_it = visionResult.getRectDetectionList().iterator();
+        while (rd_it.hasNext()) {
+            RectangularDetection rd = rd_it.next();
+            Iterator<FacialRecognition> fr_it = rd.getFacialRecognitionList().iterator();
+            while (fr_it.hasNext()) {
+                FacialRecognition fr = fr_it.next();
+                Log.d(TAG, "age:" + fr.getAge());
+                Log.d(TAG, "gender:" + fr.getGender().name());
+                Log.d(TAG, "emotion:" + fr.getEmotion().name());
+                Log.d(TAG, "pose pitch:" + fr.getPosePitch());
+                Log.d(TAG, "beard:" + fr.getBeard());
+            }
+        }
+    }
 
 }
